a28f07234c1c46d5602652ae55d0b4e3dbc9dda4
Paulo Lemes <paulo-lemes@live.com>
2024-08-29 12:43:14 -0300

    Add database url env prisma config

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,14 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
+import { env } from "../env";
 
 export const prisma = new PrismaClient({
-  log: ['query'],
-})
+  log: ["query"],
+  datasources: {
+    db: {
+      url:
+        process.env.NODE_ENV === "test"
+          ? env.TEST_DATABASE_URL
+          : env.DATABASE_URL,
+    },
+  },
+});
